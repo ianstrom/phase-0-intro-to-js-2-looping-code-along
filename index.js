4b6543def7122ee7fca4690a316e98abea187315
ianstrom <iankstrom@gmail.com>
2022-10-31 13:14:20 -0700

    commit tree 12401e01a93c1baeac732748dd8b930c4da893b8

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cards = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(cards) {
+  const thankYouArray = [];
+  for (let i = 0; i < cards.length; i++){
+    thankYouArray.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`); 
+  }
+  return thankYouArray;
+}
+
+function countDown(count) {
+  while (count >= 0) {
+    console.log(count--)
+  }
+}
+
+countDown(10);

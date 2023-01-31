9b9f205545451b6f7ad471ed3babad05643831ad
ianstrom <iankstrom@gmail.com>
2023-01-31 16:24:01 -0500

    finishing course work

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, occaision) {
+    const newArray = []
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${occaision} gift!`
+        newArray.push(message)
+    }
+    return newArray;
+}
+
+function countDown(num) {
+    while(num >= 0) {
+        console.log(num)
+        num--
+    }
+}
+countDown(4)
